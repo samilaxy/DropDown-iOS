5eeb6102ebe6b5a87890cd2476ee853c214d9652
Samnoye <samuel.noye@purplewave.com>
2024-10-25 12:11:01 +0000

    add textfield

--- UIKIT-FieldDropDown/ViewController.swift
+++ UIKIT-FieldDropDown/ViewController.swift
@@ -9,11 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tf: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+ 
 
 }
 
