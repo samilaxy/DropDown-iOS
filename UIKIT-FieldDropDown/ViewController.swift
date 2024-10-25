372714c6626613e4c9cfade9ac7f4875a94825b2
Samnoye <samuelnoye7@gmail.com>
2024-10-25 12:26:34 +0000

    added a tableview

--- UIKIT-FieldDropDown/ViewController.swift
+++ UIKIT-FieldDropDown/ViewController.swift
@@ -10,20 +10,64 @@ import UIKit
 class ViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var tf: UITextField!
+    var dataSource: [String] = []
+    let transparentView = UIView()
+    let dropDownTableView = UITableView()
+    var selectedView = UIView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         tf?.delegate = self
-
+        self.view.addSubview(transparentView)
+        dataSource = ["Fitz","Samuel","Maxwell"]
     }
  
     @IBAction func tftab(_ sender: Any) {
         print("TextField was tapped!")
+        addTransparentView(frames: tf.frame)
     }
     func textFieldDidBeginEditing(_ textField: UITextField) {
             // Perform your action here
         print("TextField was tapped!")
     }
+    func addTransparentView(frames: CGRect) {
+        if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+           let window = windowScene.windows.first {
+            transparentView.frame = window.frame
+        } else {
+            transparentView.frame = self.view.frame
+        }
+    }
 }
 
+
+
+    /////table view
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataSource.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = dataSource[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        removeTransparentView()
+        if selectedView == jobCodeDropDownView {
+            selectedJobCode = dataSource[indexPath.row]
+        }
+    }
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        removeTransparentView()
+        if selectedView == jobCodeDropDownView {
+            selectedJobCode = dataSource[indexPath.row]
+        }
+    }
+}
